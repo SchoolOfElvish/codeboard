d83eeccf99aac38cf50c5ddca0acde036470d4c9
Evgenii <silaewgenii@gmail.com>
2023-01-23 22:33:30 +0300

    [COD-3] add let instead of local variable

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
+  subject(:user1) {User.create({name:'Pasha', email:'test@mail.ru', password: 'testtest'})}
+  
   it "create users" do
-    user1 = User.create({name:'Pasha', email:'test@mail.ru', password: 'testtest'})
     expect(user1).to be_persisted
   end
-
 end
