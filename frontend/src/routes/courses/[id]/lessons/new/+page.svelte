eb4c0e80f78ebae0c0ef27f364085be6ae44d064
Evgenii <silaewgenii@gmail.com>
2023-01-26 20:29:36 +0300

    [COD-19] add routing and form

---
+++ frontend/src/routes/courses/[id]/lessons/new/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+
+
+
+<div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+  <div class="mx-auto max-w-lg text-center">
+    <h1 class="text-2xl font-bold sm:text-3xl">Create lesson:</h1>
+  </div>
+
+  <form action="" class="mx-auto mt-8 mb-0 max-w-md space-y-4">
+    <div>
+      <label for="title" class="sr-only">title</label>
+
+      <div class="relative">
+        <input
+          type="string"
+          class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
+          placeholder="Enter lesson title"
+        />
+      </div>
+    </div>
+
+    <div>
+      <label for="descriptoin" class="sr-only">Desscription</label>
+      <div class="relative">
+        <input
+          type="text"
+          class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
+          placeholder="Enter lesson description"
+        />
+      </div>
+    </div>
+
+    <div class="flex items-center justify-between">
+      <button
+        type="submit"
+        class="ml-3 inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
+      >
+        Create 
+      </button>
+    </div>
+  </form>
+</div>
