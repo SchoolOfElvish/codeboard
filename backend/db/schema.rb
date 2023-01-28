4a870d4196c8bb8a4e7ffd51319ccbd1da5698d3
Evgenii <silaewgenii@gmail.com>
2023-01-28 15:22:23 +0300

    [COD-21] rubocop modified bundle and shema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,53 +12,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_25_195802) do
+ActiveRecord::Schema[7.0].define(version: 20_230_125_195_802) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "blacklisted_tokens", force: :cascade do |t|
-    t.string "jti"
-    t.bigint "user_id", null: false
-    t.datetime "exp"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["jti"], name: "index_blacklisted_tokens_on_jti", unique: true
-    t.index ["user_id"], name: "index_blacklisted_tokens_on_user_id"
+  create_table 'blacklisted_tokens', force: :cascade do |t|
+    t.string 'jti'
+    t.bigint 'user_id', null: false
+    t.datetime 'exp'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['jti'], name: 'index_blacklisted_tokens_on_jti', unique: true
+    t.index ['user_id'], name: 'index_blacklisted_tokens_on_user_id'
   end
 
-  create_table "refresh_tokens", force: :cascade do |t|
-    t.string "crypted_token"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["crypted_token"], name: "index_refresh_tokens_on_crypted_token", unique: true
-    t.index ["user_id"], name: "index_refresh_tokens_on_user_id"
+  create_table 'refresh_tokens', force: :cascade do |t|
+    t.string 'crypted_token'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['crypted_token'], name: 'index_refresh_tokens_on_crypted_token', unique: true
+    t.index ['user_id'], name: 'index_refresh_tokens_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'name', default: '', null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  create_table "whitelisted_tokens", force: :cascade do |t|
-    t.string "jti"
-    t.bigint "user_id", null: false
-    t.datetime "exp"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["jti"], name: "index_whitelisted_tokens_on_jti", unique: true
-    t.index ["user_id"], name: "index_whitelisted_tokens_on_user_id"
+  create_table 'whitelisted_tokens', force: :cascade do |t|
+    t.string 'jti'
+    t.bigint 'user_id', null: false
+    t.datetime 'exp'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['jti'], name: 'index_whitelisted_tokens_on_jti', unique: true
+    t.index ['user_id'], name: 'index_whitelisted_tokens_on_user_id'
   end
 
-  add_foreign_key "blacklisted_tokens", "users"
-  add_foreign_key "refresh_tokens", "users"
-  add_foreign_key "whitelisted_tokens", "users"
+  add_foreign_key 'blacklisted_tokens', 'users'
+  add_foreign_key 'refresh_tokens', 'users'
+  add_foreign_key 'whitelisted_tokens', 'users'
 end
