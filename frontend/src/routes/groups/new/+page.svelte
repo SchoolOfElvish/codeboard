ef6dd20ecf688c2cee8f438d85056f291d1a842f
Evgenii <silaewgenii@gmail.com>
2023-01-28 23:52:06 +0300

    [COD-21] Using pnpm check and pnpm format

--- frontend/src/routes/groups/new/+page.svelte
+++ frontend/src/routes/groups/new/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-  let title = ''
-  let email = ''
+	let title = '';
+	let email = '';
 </script>
 
 <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
@@ -13,8 +13,8 @@
 			<label for="title" class="sr-only">title</label>
 			<div class="relative ">
 				<input
-          bind:value={title}
-          minlength="4"
+					bind:value={title}
+					minlength="4"
 					type="text"
 					class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm invalid:border-red-500"
 					placeholder="Enter group title"
@@ -26,7 +26,7 @@
 			<label for="descriptoin" class="sr-only">email</label>
 			<div class="relative">
 				<input
-        bind:value={email}
+					bind:value={email}
 					type="email"
 					maxlength="255"
 					class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm invalid:border-red-500"
@@ -37,7 +37,6 @@
 
 		<div class="flex items-center justify-between">
 			<button
-			
 				type="submit"
 				class="ml-3 inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
 			>
