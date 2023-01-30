ff6a45db72a511b7219af0af35110b5ac1863146
SilaEwGenii <102326057+SilaEwGenii@users.noreply.github.com>
2023-01-30 22:00:20 +0300

    [Cod-17] Create new course frontend (#16)
    * [COD-17] Add button navbar for new course
    
    * [COD-17] Add page courses/new and input form
    
    * [COD-17] bin/check and bin/format

---
+++ frontend/src/routes/courses/new/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	let title = '';
+</script>
+
+<div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+	<div class="mx-auto max-w-lg text-center">
+		<h1 class="text-2xl font-bold sm:text-3xl">Create course:</h1>
+	</div>
+
+	<form action="" class="mx-auto mt-8 mb-0 max-w-md space-y-4">
+		<div>
+			<label for="title" class="sr-only">title</label>
+			<div class="relative ">
+				<input
+					bind:value={title}
+					minlength="4"
+					type="text"
+					class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm invalid:border-red-500"
+					placeholder="Enter course title"
+				/>
+			</div>
+		</div>
+
+		<div class="flex items-center justify-between">
+			<button
+				type="submit"
+				class="inline-block rounded-lg bg-rose-500 px-5 py-3 text-sm font-medium text-white"
+			>
+				Create course
+			</button>
+		</div>
+	</form>
+</div>
