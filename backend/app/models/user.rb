bc8eecad14beea8727ce2b8adc30c3d9d8c50289
Evgenii <silaewgenii@gmail.com>
2023-01-31 23:37:53 +0300

    [COD-29] Add enum for model user

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum :role, { student: 0, teacher: 1 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
