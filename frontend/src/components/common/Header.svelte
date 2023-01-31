76f80a725a2fb50d3eb975f2a776c4205ceb2425
Set27 <matvei.2002@mail.ru>
2023-02-01 00:18:22 +0300

    [COD-26] Add profile page, add datapicker

--- frontend/src/components/common/Header.svelte
+++ frontend/src/components/common/Header.svelte
@@ -80,11 +80,99 @@
           </div>
         </div>
       </div>
-      {#if user.isAuthorized}
-        <Profile.Desktop items={userMenuItems} />
-      {:else}
-        <div class="hidden md:block">
-          <AuthenticationButtons />
+      <!-- <select -->
+      <!-- 	bind:value={$locale} -->
+      <!-- 	class="mt-1 block w-32 rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" -->
+      <!-- > -->
+      <!-- 	{#each $locales as sel_locale} -->
+      <!-- 		<option value={sel_locale}>{sel_locale}</option> -->
+      <!-- 	{/each} -->
+      <!-- </select> -->
+      <div class="hidden md:block">
+        <div class="ml-4 flex items-center md:ml-6">
+          <button
+            type="button"
+            class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+          >
+            <span class="sr-only">View notifications</span>
+            <Icon name="Bell" />
+          </button>
+
+          <!-- Profile dropdown -->
+          <div class="relative ml-3">
+            <div>
+              <button
+                type="button"
+                class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                on:click={() => {
+                  isDropdownOpen = !isDropdownOpen;
+                }}
+                id="user-menu-button"
+                aria-expanded="false"
+                aria-haspopup="true"
+              >
+                <span class="sr-only">Open user menu</span>
+                <img
+                  class="h-8 w-8 rounded-full"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+              </button>
+            </div>
+
+            <!--
+Dropdown menu, show/hide based on menu state.
+
+Entering: "transition ease-out duration-100"
+From: "transform opacity-0 scale-95"
+To: "transform opacity-100 scale-100"
+Leaving: "transition ease-in duration-75"
+From: "transform opacity-100 scale-100"
+To: "transform opacity-0 scale-95"
+-->
+            <Transition
+              show={isDropdownOpen}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <div
+                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="user-menu-button"
+                tabindex="-1"
+              >
+                <!-- Active: "bg-gray-100", Not Active: "" -->
+                <a
+                  href="/users/me"
+                  class="block px-4 py-2 text-sm text-gray-700"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="user-menu-item-0">Your Profile</a
+                >
+
+                <a
+                  href="#"
+                  class="block px-4 py-2 text-sm text-gray-700"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="user-menu-item-1">Settings</a
+                >
+
+                <a
+                  href="#"
+                  class="block px-4 py-2 text-sm text-gray-700"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="user-menu-item-2">Sign out</a
+                >
+              </div>
+            </Transition>
+          </div>
         </div>
       {/if}
       <div class="-mr-2 flex md:hidden">
--- frontend/src/routes/users/me/+page.svelte
+++ frontend/src/routes/users/me/+page.svelte
@@ -2,21 +2,7 @@
 	
 </script>
 
-<!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-<div class="lg:grid lg:grid-cols-12 lg:gap-x-5">
+<div class="lg:grid lg:grid-cols-12 lg:gap-x-5 container mx-auto mt-2">
   <aside class="py-6 px-2 sm:px-6 lg:col-span-3 lg:py-0 lg:px-0">
     <nav class="space-y-1">
       <!-- Current: "bg-gray-50 text-indigo-700 hover:text-indigo-700 hover:bg-white", Default: "text-gray-900 hover:text-gray-900 hover:bg-gray-50" -->
@@ -65,7 +51,7 @@
       </a>
     </nav>
   </aside>
-
+  
   <div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0">
     <form action="#" method="POST">
       <div class="shadow sm:overflow-hidden sm:rounded-md">
@@ -74,199 +60,31 @@
             <h3 class="text-lg font-medium leading-6 text-gray-900">Profile</h3>
             <p class="mt-1 text-sm text-gray-500">This information will be displayed publicly so be careful what you share.</p>
           </div>
-
-          <div class="grid grid-cols-3 gap-6">
-            <div class="col-span-3 sm:col-span-2">
-              <label for="company-website" class="block text-sm font-medium text-gray-700">Username</label>
-              <div class="mt-1 flex rounded-md shadow-sm">
-                <span class="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm">workcation.com/</span>
-                <input type="text" name="username" id="username" autocomplete="username" class="block w-full min-w-0 flex-grow rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-              </div>
-            </div>
-
-            <div class="col-span-3">
-              <label for="about" class="block text-sm font-medium text-gray-700">About</label>
-              <div class="mt-1">
-                <textarea id="about" name="about" rows="3" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="you@example.com"></textarea>
-              </div>
-              <p class="mt-2 text-sm text-gray-500">Brief description for your profile. URLs are hyperlinked.</p>
-            </div>
-
-            <div class="col-span-3">
-              <label class="block text-sm font-medium text-gray-700">Photo</label>
-              <div class="mt-1 flex items-center">
-                <span class="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
-                  <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                  </svg>
-                </span>
-                <button type="button" class="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Change</button>
-              </div>
-            </div>
-
-            <div class="col-span-3">
-              <label class="block text-sm font-medium text-gray-700">Cover photo</label>
-              <div class="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                <div class="space-y-1 text-center">
-                  <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                  <div class="flex text-sm text-gray-600">
-                    <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
-                      <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" class="sr-only">
-                    </label>
-                    <p class="pl-1">or drag and drop</p>
-                  </div>
-                  <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-          <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
-        </div>
-      </div>
-    </form>
-
-    <form action="#" method="POST">
-      <div class="shadow sm:overflow-hidden sm:rounded-md">
-        <div class="space-y-6 bg-white py-6 px-4 sm:p-6">
-          <div>
-            <h3 class="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
-            <p class="mt-1 text-sm text-gray-500">Use a permanent address where you can recieve mail.</p>
-          </div>
-
-          <div class="grid grid-cols-6 gap-6">
-            <div class="col-span-6 sm:col-span-3">
-              <label for="first-name" class="block text-sm font-medium text-gray-700">First name</label>
-              <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-            </div>
-
-            <div class="col-span-6 sm:col-span-3">
-              <label for="last-name" class="block text-sm font-medium text-gray-700">Last name</label>
-              <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-            </div>
-
-            <div class="col-span-6 sm:col-span-4">
-              <label for="email-address" class="block text-sm font-medium text-gray-700">Email address</label>
-              <input type="text" name="email-address" id="email-address" autocomplete="email" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-            </div>
-
-            <div class="col-span-6 sm:col-span-3">
-              <label for="country" class="block text-sm font-medium text-gray-700">Country</label>
-              <select id="country" name="country" autocomplete="country-name" class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-                <option>United States</option>
-                <option>Canada</option>
-                <option>Mexico</option>
-              </select>
-            </div>
-
-            <div class="col-span-6">
-              <label for="street-address" class="block text-sm font-medium text-gray-700">Street address</label>
-              <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-            </div>
-
-            <div class="col-span-6 sm:col-span-6 lg:col-span-2">
-              <label for="city" class="block text-sm font-medium text-gray-700">City</label>
-              <input type="text" name="city" id="city" autocomplete="address-level2" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-            </div>
-
-            <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-              <label for="region" class="block text-sm font-medium text-gray-700">State / Province</label>
-              <input type="text" name="region" id="region" autocomplete="address-level1" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-            </div>
-
-            <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-              <label for="postal-code" class="block text-sm font-medium text-gray-700">ZIP / Postal code</label>
-              <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-            </div>
-          </div>
-        </div>
-        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-          <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
-        </div>
-      </div>
-    </form>
-
-    <form action="#" method="POST">
-      <div class="shadow sm:overflow-hidden sm:rounded-md">
-        <div class="space-y-6 bg-white py-6 px-4 sm:p-6">
-          <div>
-            <h3 class="text-lg font-medium leading-6 text-gray-900">Notifications</h3>
-            <p class="mt-1 text-sm text-gray-500">Provide basic informtion about the job. Be specific with the job title.</p>
-          </div>
-
-          <fieldset>
-            <legend class="text-base font-medium text-gray-900">By Email</legend>
-            <div class="mt-4 space-y-4">
-              <div class="flex items-start">
-                <div class="flex h-5 items-center">
-                  <input id="comments" name="comments" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                </div>
-                <div class="ml-3 text-sm">
-                  <label for="comments" class="font-medium text-gray-700">Comments</label>
-                  <p class="text-gray-500">Get notified when someones posts a comment on a posting.</p>
-                </div>
-              </div>
+            
+        <div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0">
+          <div class="shadow sm:overflow-hidden sm:rounded-md">
+            <div class="space-y-6 bg-white py-6 px-4 sm:p-6">
               <div>
-                <div class="flex items-start">
-                  <div class="flex h-5 items-center">
-                    <input id="candidates" name="candidates" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
+                <h3 class="text-lg font-medium leading-6 text-gray-900">Input your birthday date</h3>
+                <form>
+                  <div class="date-picker">
+                    <input class="mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" type="date"  id="party" name="party" min="1900-01-01" max="2099-01-01" required>
+                    <div class = "inline-block">
+                      <button type="submit"
+                              class="mt-1 inline-block rounded-lg bg-green-500 px-5 py-3 text-sm font-medium text-white invalid:border-red-500">
+                        <span class = "text-date-picker">Submit</span>
+                      </button>
+                    </div>
                   </div>
-                  <div class="ml-3 text-sm">
-                    <label for="candidates" class="font-medium text-gray-700">Candidates</label>
-                    <p class="text-gray-500">Get notified when a candidate applies for a job.</p>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <div class="flex items-start">
-                  <div class="flex h-5 items-center">
-                    <input id="offers" name="offers" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                  </div>
-                  <div class="ml-3 text-sm">
-                    <label for="offers" class="font-medium text-gray-700">Offers</label>
-                    <p class="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
-                  </div>
-                </div>
+                </form>
               </div>
             </div>
-          </fieldset>
-          <fieldset class="mt-6">
-            <legend class="text-base font-medium text-gray-900">Push Notifications</legend>
-            <p class="text-sm text-gray-500">These are delivered via SMS to your mobile phone.</p>
-            <div class="mt-4 space-y-4">
-              <div class="flex items-center">
-                <input id="push-everything" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                <label for="push-everything" class="ml-3">
-                  <span class="block text-sm font-medium text-gray-700">Everything</span>
-                </label>
-              </div>
-              <div class="flex items-center">
-                <input id="push-email" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                <label for="push-email" class="ml-3">
-                  <span class="block text-sm font-medium text-gray-700">Same as email</span>
-                </label>
-              </div>
-              <div class="flex items-center">
-                <input id="push-nothing" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                <label for="push-nothing" class="ml-3">
-                  <span class="block text-sm font-medium text-gray-700">No push notifications</span>
-                </label>
-              </div>
-            </div>
-          </fieldset>
-        </div>
-        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-          <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2">Save</button>
+          </div>
         </div>
       </div>
-    </form>
   </div>
 </div>
-
 <!-- <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
   <div class="mx-auto max-w-lg text-center">
     <h2 class="text-2xl font-bold sm:text-3xl">Input your birthday date</h2>
@@ -282,6 +100,7 @@
       </div>
     </form>
   </div>
-</div>
- -->
+</div> -->
+
+
 
