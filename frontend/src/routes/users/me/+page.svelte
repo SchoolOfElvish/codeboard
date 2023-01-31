f1f656426a55b8d2240d450cfe6e5be0af90c846
Set27 <matvei.2002@mail.ru>
2023-01-31 22:03:55 +0300

    [COD-26] Add datepicker

--- frontend/src/routes/users/me/+page.svelte
+++ frontend/src/routes/users/me/+page.svelte
@@ -1,1 +1,21 @@
-<h1>Here will be display user profile page</h1>
+<script lang="ts">
+	
+</script>
+
+<div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+  <div class="mx-auto max-w-lg text-center">
+    <h2 class="text-2xl font-bold sm:text-3xl">Input your birthday date</h2>
+    <form>
+      <div>
+        <input type="date" id="party" name="party" min="2017-04-01" max="2099-04-20" required>
+        <span class="validity"></span>
+      </div>
+      <div>
+        <button type="submit"
+                class=" rounded-md bg-rose-600 px-5 py-3 text-white transition focus:outline-none focus:ring focus:ring-yellow-400 sm:mt-0 sm:w-auto">
+          dsad
+        </button>
+      </div>
+    </form>
+  </div>
+</div>
