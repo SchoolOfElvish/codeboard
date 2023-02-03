d70efbd651de25bace1c1d50ff368fb1f21de303
Mikhail Zaitsau <mikhail_zaitsau@outlook.com>
2023-02-03 23:48:48 +0300

    [COD-18] Add request spec (RED)

--- backend/spec/requests/api/v1/courses_spec.rb
+++ backend/spec/requests/api/v1/courses_spec.rb
@@ -1,7 +1,33 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Courses", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Api::V1::Courses', type: :request do
+  describe 'POST /create' do
+    before do
+      sign_in user
+      # login_user
+    end
+
+    let(:user) { create(:user) }
+    let(:name) { 'TestCourse' }
+    let(:params) { { name: } }
+
+    it 'create a new courese' do
+      post('/api/v1/courses', params:)
+      expect(response).to have_http_status(:created)
+    end
   end
 end
+
+# require 'rails_helper'
+
+# RSpec.describe 'Api::V1::Courses', type: :request do
+#   describe "POST /new" do
+#     it "returns http success" do
+#       post '/api/v1/courses/new'
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+# end 
