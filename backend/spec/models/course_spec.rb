986bd92e6e5bf8cf1be800c48c84a093fd4f3158
Mikhail Zaitsau <mikhail_zaitsau@outlook.com>
2023-02-03 21:33:05 +0300

    [COD-18] Adding model test (GREEN)

--- backend/spec/models/course_spec.rb
+++ backend/spec/models/course_spec.rb
@@ -1,5 +1,25 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:testCourse) { described_class.create(course_params) }
+
+  let(:current_user) { User.create({ name: 'Test', email: 'test@mail.ru', password: 'testtest' }) }
+  let(:name) { 'TestCourse' }
+  let(:user_id) { current_user.id }
+  let(:course_params) { { name:, user_id:} }
+
+  it 'creates a course attached to current user' do
+    expect(testCourse).to be_persisted
+    expect(testCourse.user_id).to eq(current_user.id)
+  end
+
+  context 'name is empty' do
+    let(:name) { '' }
+
+    it "dosen't create a new course" do
+      expect(testCourse).to_not be_persisted
+    end
+  end
 end
