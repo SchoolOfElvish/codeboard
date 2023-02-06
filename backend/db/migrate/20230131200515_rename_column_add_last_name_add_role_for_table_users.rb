23bc2cb43078f3ee4dc46e602c043f52a762c195
Evgenii <silaewgenii@gmail.com>
2023-02-06 20:24:56 +0300

    [COD-29] Add bulk:true for table users

--- backend/db/migrate/20230131200515_rename_column_add_last_name_add_role_for_table_users.rb
+++ backend/db/migrate/20230131200515_rename_column_add_last_name_add_role_for_table_users.rb
@@ -2,7 +2,7 @@
 
 class RenameColumnAddLastNameAddRoleForTableUsers < ActiveRecord::Migration[7.0]
   def change
-    change_table :users do |t|
+    change_table :users, bulk: true do |t|
       t.rename :name, :first_name
       t.string :last_name,           null: false, default: ''
       t.integer :role,               default: 0
