673e35723d3f84682651af073df3d49b175de0bd
Mikhail Zaitsau <mikhail_zaitsau@outlook.com>
2023-02-07 22:21:56 +0300

    [COD-35] Include RequestSpecHelper module in spec_helper, fixed some syntax errors

--- backend/spec/support/request_spec_helper.rb
+++ backend/spec/support/request_spec_helper.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 module RequestSpecHelper
   include FactoryBot::Syntax::Methods
 
   def auth_user
     user = create(:user)
     token = Jwt::Issuer.call(user)[0]
-    return {'Authorization' => "Bearer #{token}"}
+    { 'Authorization' => "Bearer #{token}" }
   end
 end
