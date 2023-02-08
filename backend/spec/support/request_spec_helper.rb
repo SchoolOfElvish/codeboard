69bbc725b92f1a0ad5cbc0253ef9f32f5637a873
Mikhail Zaitsau <mikhail_zaitsau@outlook.com>
2023-02-08 19:52:27 +0300

    [COD-35] Code refactoring: new method name - auth_header_for, receive user as an argument

--- backend/spec/support/request_spec_helper.rb
+++ backend/spec/support/request_spec_helper.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 module RequestSpecHelper
-  def auth_user
-    user = create(:user)
+  def auth_header_for(user)
     token = Jwt::Issuer.call(user)[0]
     { 'Authorization' => "Bearer #{token}" }
   end
