18b9ba732b3b1c70a6e7a1ddf6d1725ef4ba2760
Set27 <matvei.2002@mail.ru>
2023-02-08 21:36:13 +0300

    [COD-26] Docker fix

--- backend/app/services/users/register.rb
+++ backend/app/services/users/register.rb
@@ -11,7 +11,7 @@ module Users
 
     private
 
-    attr_reader :email, :password, :password_confirmation
+    attr_reader :email, :password, :password_confirmation, :user_params
 
     def create_user
       user = User.new(user_info)
@@ -33,7 +33,5 @@ module Users
       token, refresh_token = Jwt::Issuer.call(user)
       Success[token, refresh_token.token]
     end
-
-    attr_reader :user_params
   end
 end
