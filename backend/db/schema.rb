b751b4020c805704c11b0a64b5cc519b81f41db7
Pavel <lordevildead@gmail.com>
2023-02-20 13:58:31 +0300

    [COD-22] fixed for github db:prepare

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_07_180204) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +30,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_180204) do
   end
 
 
-
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.bigint "user_id", null: false
@@ -39,14 +39,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_180204) do
   end
 
   
-e3cec5f ([COD-22] final test creation+fixcontroller&model)
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
 
- e3cec5f ([COD-22] final test creation+fixcontroller&model)
   end
 
 
