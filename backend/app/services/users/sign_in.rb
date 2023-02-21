f32796425b4e05209d2353f54e0c93770bb55372
Set27 <matvei.2002@mail.ru>
2023-02-22 01:09:44 +0300

    [COD-14] Backend logic v1

--- backend/app/services/users/sign_in.rb
+++ backend/app/services/users/sign_in.rb
@@ -4,15 +4,29 @@ module Users
   class SignIn < Core::Service
     def call(user_params)
       @user_params = user_params
+      @email = user_params[:email]
       @password = user_params[:password]
+      @user = find_user(@email)
+      validate_password(@password)
+      token, refresh_token = yield issue_token(user)
+      Success[token, refresh_token]
     end
 
     private
-
-    def validate_password
-      user.valid_password?(password)
+    
+    def find_user(email)
+      user = User.find_by(email)
+      user ? Success(user) : Failure(user.errors) 
     end
 
+    def validate_password(password)
+      @user.valid_password?(password)
+    end
+
+    def issue_token(user)
+      token, refresh_token = Jwt::Issuer.call(user)
+      Success[token, refresh_token.token]
+    end
 
   end
 end
