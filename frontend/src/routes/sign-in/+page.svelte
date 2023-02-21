a1c822ff0d2d67260125501eaeda93ea0ed0f85c
Set27 <matvei.2002@mail.ru>
2023-02-22 02:09:52 +0300

    [COD-14] Delete page centering, increse py from 12 to 20

--- frontend/src/routes/sign-in/+page.svelte
+++ frontend/src/routes/sign-in/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+<div class="flex min-h-full flex-col py-20 sm:px-6 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-md">
     <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
     <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
@@ -36,6 +36,7 @@
           <button type="submit" class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Sign in</button>
         </div>
       </form>
+
     </div>
   </div>
 </div>
