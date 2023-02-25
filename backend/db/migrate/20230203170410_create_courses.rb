07a3a107798cfead00af4c8d09aff91c2dafb4ef
Mikhail Zaitsau <mikhail_zaitsau@outlook.com>
2023-02-26 02:34:57 +0300

    [COD-18] Fixed all issues

--- backend/db/migrate/20230203170410_create_courses.rb
+++ backend/db/migrate/20230203170410_create_courses.rb
@@ -3,7 +3,7 @@
 class CreateCourses < ActiveRecord::Migration[7.0]
   def change
     create_table :courses do |t|
-      t.string :title
+      t.string :name
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- backend/db/migrate/20230203180846_fix_column_name_in_courses.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class FixColumnNameInCourses < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :courses, :title, :name
-  end
-end
