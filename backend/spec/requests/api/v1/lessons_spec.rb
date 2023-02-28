19a43546f7a63b968085876f6a08ad7ccebfd198
Evgenii <silaewgenii@gmail.com>
2023-03-01 00:12:53 +0300

    [COD-20] Add request full test , not testing models

--- backend/spec/requests/api/v1/lessons_spec.rb
+++ backend/spec/requests/api/v1/lessons_spec.rb
@@ -2,16 +2,25 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Api::V1::Lessons', type: :request do
+RSpec.describe 'Api::V1::Lessons' do
   describe 'POST /api/v1/courses/:course_id/lessons' do
     let(:user) { create(:user) }
     let(:headers) { auth_header_for(user) }
-    let(:course) { create(:course, name: 'Test Course') }
-    let(:params) { { course_id: course.id, title: 'dsa' } }
+    let(:course) { create(:course, name: 'TestCourse') }
+    let(:params) { { course_id: course.id, title: 'TestTitle', description: 'TestDescription' } }
 
     it 'creates a new lesson' do
-      post "/api/v1/courses/#{course.id}/lessons", params: params, headers: headers
+      post("/api/v1/courses/#{course.id}/lessons", params:, headers:)
       expect(response).to have_http_status(:created)
+    end
+
+    context 'when name is empty' do
+      let(:params) { { course_id: course.id, title: '', description: 'dsad' } }
+
+      it "dosen't create a new lessom" do
+        post("/api/v1/courses/#{course.id}/lessons", params:, headers:)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
     end
   end
 end
