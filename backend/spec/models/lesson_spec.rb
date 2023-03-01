41bf1b3d69c51710297bad3dee17f29f5698f6eb
Evgenii <silaewgenii@gmail.com>
2023-03-01 21:14:10 +0300

    [COD-20] Add test for model lesson and add gem shoulda

--- backend/spec/models/lesson_spec.rb
+++ backend/spec/models/lesson_spec.rb
@@ -3,5 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe Lesson do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:lesson) do
+    described_class.new(title: 'Lesson Title',
+                        description: 'Lesson Description',
+                        course:)
+  end
+
+  let(:user) { create(:user) }
+  let(:course) { Course.create(name: 'Course Name', user:) }
+
+  describe 'validations' do
+    it 'valid value' do
+      expect(lesson).to be_valid
+    end
+
+    it 'not a valid title' do
+      lesson.title = nil
+      expect(lesson).not_to be_valid
+    end
+
+    it 'not a valid description' do
+      lesson.description = nil
+      expect(lesson).not_to be_valid
+    end
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:course) }
+  end
 end
