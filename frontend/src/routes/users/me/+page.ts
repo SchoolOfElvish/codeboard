8314c4fef2e09ed7d874efc13da25e7c324f79e4
Set27 <matvei.2002@mail.ru>
2023-03-02 22:43:22 +0300

    [COD-25] Delete all unnecessary comment from users/me page, some minor fronted fixes

--- frontend/src/routes/users/me/+page.ts
+++ frontend/src/routes/users/me/+page.ts
@@ -1,20 +1,15 @@
 import type { PageLoad } from './$types';
-import {get} from '$utils/fetch';
-
-let firstName: string;
-let lastName: string;
-let email: string;
-let birthdate: string;
+import { get } from '$utils/fetch';
 
 type ResponseData = {
   firstName: string;
   lastName: string;
   email: string;
   birthdate: string;
-}
+};
 
-export const load = ( async () => {
+export const load = (async () => {
   const response = await (await get('/v1/users/me')).json<ResponseData>();
-  
-  return {response};
+
+  return { response };
 }) satisfies PageLoad;
