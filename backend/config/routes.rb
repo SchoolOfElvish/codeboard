ab5075dab6be422e3bf124728e9ce55d0494aeb3
Nick Pupko <work.pupko@gmail.com>
2023-03-06 14:00:26 +0300

    Remove unused commentary

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,10 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api do
     namespace :v1 do
       post '/sign-up', to: 'sessions#sign_up'
