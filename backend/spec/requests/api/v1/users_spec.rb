77191952b869de6e11ebfd26fdf537de2ff3d473
Set27 <matvei.2002@mail.ru>
2023-03-07 17:24:15 +0300

    [COD-25] Update rspec for service update_user_info
    [COD-25] Try to refactor request test
    
    [COD-25] update service and request tests

--- backend/spec/requests/api/v1/users_spec.rb
+++ backend/spec/requests/api/v1/users_spec.rb
@@ -4,28 +4,72 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Users' do
   describe 'PUT /me' do
-    let(:params) do
-      { user:, birthdate: }
+    let(:user_params) do
+      {
+        first_name: user.first_name,
+        last_name: user.last_name,
+        birthdate: nil
+      }
     end
-    let(:password_confirmation) { password }
-    let(:birthdate) { nil }
-    let!(:user) { create(:user) }
+
+    let(:user) { create(:user) }
+    let(:headers) { auth_header_for(user) }
 
     context 'when birthdate is not nil' do
-      it 'returns an 204' do
-        pending 'Refactoring now'
-        put('/api/v1/users/me', headers: auth_header_for(user), params:)
+      it 'returns an 200' do
+        # binding.pry
+        put('/api/v1/users/me', params: user_params, headers:)
         expect(response).to have_http_status(:no_content)
       end
     end
 
     context 'when birthdate is provided' do
-      let(:birthdate) { Time.zone.today }
+      let(:user_params) do
+        {
+          first_name: user.first_name,
+          last_name: user.last_name,
+          birthdate: Time.zone.today
+        }
+      end
 
       it 'updates the user birthdate' do
-        pending 'Refactoring now'
-        put('/api/v1/users/me', headers: auth_header_for(user), params:)
-        expect(user.reload.birthdate).to eq(birthdate)
+        put('/api/v1/users/me', params: user_params, headers:)
+        expect(user.reload.birthdate).to eq(user_params[:birthdate])
+      end
+    end
+
+    context 'when user provide new valid full name' do
+      let(:user_params) do
+        {
+          first_name: 'Newname',
+          last_name: 'Newname',
+          birthdate: nil
+        }
+      end
+
+      it 'update first name' do
+        put('/api/v1/users/me', params: user_params, headers:)
+        expect(user.reload.first_name).to eq(user_params[:first_name])
+      end
+
+      it 'update last name' do
+        put('/api/v1/users/me', params: user_params, headers:)
+        expect(user.reload.last_name).to eq(user_params[:last_name])
+      end
+    end
+
+    context 'when user provide nil full name' do
+      let(:user_params) do
+        {
+          first_name: nil,
+          last_name: nil,
+          birthdate: nil
+        }
+      end
+
+      it 'return error' do
+        put('/api/v1/users/me', params: user_params, headers:)
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
