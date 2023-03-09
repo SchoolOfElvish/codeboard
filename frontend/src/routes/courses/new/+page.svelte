8e926e6c920a02f2dc15a6e6e285097971077779
Evgenii <silaewgenii@gmail.com>
2023-03-09 22:15:06 +0300

    [COD-39] Connecting frontend and backend

--- frontend/src/routes/courses/new/+page.svelte
+++ frontend/src/routes/courses/new/+page.svelte
@@ -1,5 +1,15 @@
-<script>
-  let title = '';
+<script lang="ts">
+  
+  import { post } from '$utils/fetch';
+  
+  let name = '';
+  
+  const handleSubmit = async () => {
+    const response = await post('/v1/courses', { name });
+    
+  };
+  
+  
 </script>
 
 <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
@@ -7,12 +17,12 @@
     <h1 class="text-2xl font-bold sm:text-3xl">Create course:</h1>
   </div>
 
-  <form action="" class="mx-auto mt-8 mb-0 max-w-md space-y-4">
+  <form  class="mx-auto mt-8 mb-0 max-w-md space-y-4">
     <div>
       <label for="title" class="sr-only">title</label>
       <div class="relative ">
         <input
-          bind:value={title}
+          bind:value={name}
           minlength="4"
           type="text"
           class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm invalid:border-red-500"
@@ -23,6 +33,7 @@
 
     <div class="flex items-center justify-between">
       <button
+        on:click|preventDefault={handleSubmit}
         type="submit"
         class="inline-block rounded-lg bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
       >
