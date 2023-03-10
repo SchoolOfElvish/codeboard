7176d12b3b87d03acd06fc6d5be21bc79a17f167
Mikhail Zaitsau <mikhail_zaitsau@outlook.com>
2023-03-10 23:18:34 +0300

    [COD-30] Working on sing_out service

--- backend/app/services/users/sign_out.rb
+++ backend/app/services/users/sign_out.rb
@@ -2,10 +2,22 @@
 
 module Users
   class SignOut < Core::Service
-    def call
+    def call(user:, token:)
+      @user = user
+      @token = token
+
     end
 
     private
 
+    def decode_token
+      Success(Jwt::Decoder.decode!(token, verify: false))
+    rescue StandardError
+      Failure(:token_cannot_be_decoded)
+    end
+
+    def logout()
+
+    end
   end
 end
