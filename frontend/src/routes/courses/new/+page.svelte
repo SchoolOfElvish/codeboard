b739f49a0dbf4fe838b6afc4c7636c525ad742a5
Evgenii <silaewgenii@gmail.com>
2023-03-14 00:41:03 +0300

    [COD-39] Add form for 422 respons

--- frontend/src/routes/courses/new/+page.svelte
+++ frontend/src/routes/courses/new/+page.svelte
@@ -4,47 +4,77 @@
 
   let name = '';
   let isSuccess = false;
+  let errorMessage = '';
 
   const handleSubmit = async () => {
-    if (name.length >= 4) {
-      const response = await post('/v1/courses', { name });
+    const result = await post('/v1/courses', { name });
+    const response = await result.res();
 
-      if (response) {
-        isSuccess = true;
-      }
+    if (response.status === 201) {
+      isSuccess = true;
+    } else if (response.status === 422) {
+      isSuccess = true;
+      errorMessage = response.error.message;
     }
   };
 
   const closeAlert = () => {
     isSuccess = false;
+    errorMessage = '';
   };
 </script>
 
 <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-  {#if isSuccess == true}
-    <div class="rounded-md bg-green-50 p-4">
-      <div class="flex">
-        <div class="flex-shrink-0">
-          <Icon name="CheckCircle" />
-        </div>
-        <div class="ml-3">
-          <p class="text-sm font-medium text-green-800">Course created!</p>
-        </div>
-        <div class="ml-auto pl-3">
-          <div class="-mx-1.5 -my-1.5">
-            <button
-              type="button"
-              class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
-              on:click|preventDefault={closeAlert}
-            >
-              <span class="sr-only">Dismiss</span>
-              <Icon name="XMark" />
-            </button>
+  {#if isSuccess}
+    {#if errorMessage != ''}
+      <div class="rounded-md bg-red-50 p-4">
+        <div class="flex">
+          <div class="flex-shrink-0">
+            <Icon name="XCircle" />
+          </div>
+          <div class="ml-3">
+            <p class="text-sm font-medium text-red-800">{errorMessage}</p>
+          </div>
+          <div class="ml-auto pl-3">
+            <div class="-mx-1.5 -my-1.5">
+              <button
+                type="button"
+                class="inline-flex rounded-md bg-red-50 p-1.5 text-red-500 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-red-50"
+                on:click|preventDefault={closeAlert}
+              >
+                <span class="sr-only">Dismiss</span>
+                <Icon name="XMark" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    {:else}
+      <div class="rounded-md bg-green-50 p-4">
+        <div class="flex">
+          <div class="flex-shrink-0">
+            <Icon name="CheckCircle" />
+          </div>
+          <div class="ml-3">
+            <p class="text-sm font-medium text-green-800">Course created!</p>
+          </div>
+          <div class="ml-auto pl-3">
+            <div class="-mx-1.5 -my-1.5">
+              <button
+                type="button"
+                class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
+                on:click|preventDefault={closeAlert}
+              >
+                <span class="sr-only">Dismiss</span>
+                <Icon name="XMark" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    {/if}
   {/if}
+
   <div class="mx-auto max-w-lg text-center">
     <h1 class="text-2xl font-bold sm:text-3xl">Create course:</h1>
   </div>
@@ -55,7 +85,6 @@
       <div class="relative ">
         <input
           bind:value={name}
-          minlength="4"
           type="text"
           class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm invalid:border-red-500"
           placeholder="Enter course title"
