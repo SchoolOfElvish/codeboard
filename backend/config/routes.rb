cdb8cd099a3b44f335b68fd20a8e0050fd7865a0
Pavel <lordevildead@gmail.com>
2023-03-13 19:47:11 +0300

    [COD-22] iverse_of fix & deleted course route [Backend]

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
       resources :courses, only: [:create] do
         resources :lessons, only: [:create]
       end
-      resources :courses, only: [:create]
       resources :groups, only: %i[create]
     end
   end
