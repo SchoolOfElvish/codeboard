9be6fc13f71ea7cc5d2011f92c36dc019f6772f4
Evgenii <silaewgenii@gmail.com>
2023-03-15 00:29:47 +0300

    [COD-39] Added localization for the course creation page

--- frontend/src/routes/courses/new/+page.svelte
+++ frontend/src/routes/courses/new/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import Icon from '$components/icons/Icon.svelte';
   import { post } from '$utils/fetch';
-  type OperationStatus = 'success' | 'failure' | 'incompleted';
+  import { _ } from 'svelte-i18n';
+
   let name = '';
 
   let status: OperationStatus = 'incompleted';
   let errors: Error = {};
+
+  type OperationStatus = 'success' | 'failure' | 'incompleted';
   type Error = {
     [key: string]: string[];
   };
@@ -17,9 +20,8 @@
       .error(422, async (error) => {
         errors = JSON.parse(error.message).errors;
         status = 'failure';
-        console.log(JSON.parse(error.message))
+        console.log(JSON.parse(error.message));
         return error;
-        
       })
       .res(() => (status = 'success'));
   };
@@ -33,10 +35,16 @@
   {#if status === 'failure'}
     {#if Object.keys(errors).length > 0}
       <div role="alert" class="rounded border-l-4 border-red-500 bg-red-50 p-4">
-        <strong class="block font-medium text-red-700"> Something went wrong </strong>
+        <strong class="block font-medium text-red-700"
+          >{$_('pages.create_course.something_went_wrong')}</strong
+        >
         <p class="mt-2 text-sm text-red-700">
           {#each Object.keys(errors) as key}
-            {key}: {errors[key].join(', ')}
+            {#if key === 'name'}
+              {$_('pages.create_course.errors.name')}
+            {:else}
+              {key}: {errors[key].join(', ')}
+            {/if}
             <br />
           {/each}
         </p>
@@ -49,7 +57,9 @@
           <Icon name="CheckCircle" />
         </div>
         <div class="ml-3">
-          <p class="text-sm font-medium text-green-800">Successfully uploaded</p>
+          <p class="text-sm font-medium text-green-800">
+            {$_('pages.create_course.success_message')}
+          </p>
         </div>
         <div class="ml-auto pl-3">
           <div class="-mx-1.5 -my-1.5">
@@ -68,7 +78,7 @@
   {/if}
 
   <div class="mx-auto max-w-lg text-center">
-    <h1 class="text-2xl font-bold sm:text-3xl">Create course:</h1>
+    <h1 class="text-2xl font-bold sm:text-3xl">{$_('pages.create_course.title')}</h1>
   </div>
 
   <form class="mx-auto mt-8 mb-0 max-w-md space-y-4">
@@ -79,7 +89,7 @@
           bind:value={name}
           type="text"
           class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm invalid:border-red-500"
-          placeholder="Enter course title"
+          placeholder={$_('pages.create_course.input_label')}
         />
       </div>
     </div>
@@ -90,7 +100,7 @@
         type="submit"
         class="inline-block rounded-lg bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
       >
-        Create course
+        {$_('pages.create_course.create_button')}
       </button>
     </div>
   </form>
