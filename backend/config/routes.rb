a1b71fad84be7e9630131337cc87792ad670f7cc
Pavel <lordevildead@gmail.com>
2023-03-16 23:20:34 +0300

    [COD-38] fix group rout & connect backend and frontend [frontend]

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
       resources :courses, only: [:create] do
         resources :lessons, only: [:create]
       end
-      resources :groups, only: %i[create]
+      resources :groups, only: [:create]
     end
   end
 end
--- frontend/src/routes/groups/new/+page.svelte
+++ frontend/src/routes/groups/new/+page.svelte
@@ -1,6 +1,34 @@
-<script>
-  let title = '';
-  let email = '';
+<script lang="ts">
+  import { post } from '$utils/fetch';
+  import Icon from '$components/icons/Icon.svelte';
+
+  type OperationStatus = 'success' | 'failure' | 'incompleted';
+
+  let title: string;
+  let email: string;
+  let status: OperationStatus = 'incompleted';
+  let errors: Error = {};
+  type Error = {
+    [key: string]: string[];
+  };
+
+  const createGroup = async () => {
+    const result = await post('/v1/groups', {
+      name: title
+    });
+
+    result
+      .error(422, async (error) => {
+        errors = JSON.parse(error.message).error;
+        status = 'failure';
+        return error;
+      })
+      .res(() => (status = 'success'));
+  };
+
+  const closeAlert = () => {
+    status = 'incompleted';
+  };
 </script>
 
 <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
@@ -35,13 +63,53 @@
       </div>
     </div>
 
-    <div class="flex items-center justify-between">
-      <button
-        type="submit"
-        class="inline-block rounded-lg bg-rose-500 px-5 py-3 text-sm font-medium text-white"
-      >
-        Create Group
-      </button>
+    <div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0">
+      {#if status === 'failure'}
+        {#if Object.keys(errors).length > 0}
+          <div role="alert" class="rounded border-l-4 border-red-500 bg-red-50 p-4">
+            <strong class="block font-medium text-red-700"> Something went wrong </strong>
+            <p class="mt-2 text-sm text-red-700">
+              {#each Object.keys(errors) as key}
+                {key}: {errors[key].join(', ')}
+                <br />
+              {/each}
+            </p>
+          </div>
+        {/if}
+      {:else if status === 'success'}
+        <div class="rounded-md bg-green-100 p-4">
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <Icon name="CheckCircle" />
+            </div>
+            <div class="ml-3">
+              <p class="text-sm font-medium text-green-800">Successfully created</p>
+            </div>
+            <div class="ml-auto pl-3">
+              <div class="-mx-1.5 -my-1.5">
+                <button
+                  type="button"
+                  class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
+                  on:click|preventDefault={closeAlert}
+                >
+                  <span class="sr-only">Dismiss</span>
+                  <Icon name="XMark" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      {/if}
+
+      <div class="flex items-center justify-between">
+        <button
+          type="submit"
+          on:click={createGroup}
+          class="inline-block rounded-lg bg-rose-500 px-5 py-3 text-sm font-medium text-white"
+        >
+          Create Group
+        </button>
+      </div>
     </div>
   </form>
 </div>
