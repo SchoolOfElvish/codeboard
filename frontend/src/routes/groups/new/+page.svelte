7c3fb39709f5217a66ab50f97275179031c73f8e
Pavel <lordevildead@gmail.com>
2023-03-17 18:23:04 +0300

    [COD-38] upd errors render to up [Frontend]

--- frontend/src/routes/groups/new/+page.svelte
+++ frontend/src/routes/groups/new/+page.svelte
@@ -28,13 +28,58 @@
       .res(() => (status = 'success'));
   };
 
-
   const closeAlert = () => {
     status = 'incompleted';
   };
 </script>
 
-<div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+<div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-18">
+  <div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0">
+    {#if status === 'failure'}
+      {#if Object.keys(errors).length > 0}
+        <div role="alert" class="rounded border-l-4 border-red-500 bg-red-50 p-4">
+          <strong class="block font-medium text-red-700">
+            {$_('pages.create_group.something_went_wrong')}
+          </strong>
+          <p class="mt-2 text-sm text-red-700">
+            {#each Object.keys(errors) as key}
+              {#if key === 'name'}
+                {$_('pages.create_group.errors.title')}
+              {:else}
+                {key}: {errors[key].join(', ')}
+              {/if}
+              <br />
+            {/each}
+          </p>
+        </div>
+      {/if}
+    {:else if status === 'success'}
+      <div class="rounded-md bg-green-100 p-4">
+        <div class="flex">
+          <div class="flex-shrink-0">
+            <Icon name="CheckCircle" />
+          </div>
+          <div class="ml-3">
+            <p class="text-sm font-medium text-green-800">
+              {$_('pages.create_group.success_message')}
+            </p>
+          </div>
+          <div class="ml-auto pl-3">
+            <div class="-mx-1.5 -my-1.5">
+              <button
+                type="button"
+                class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
+                on:click|preventDefault={closeAlert}
+              >
+                <span class="sr-only">Dismiss</span>
+                <Icon name="XMark" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    {/if}
+  </div>
   <div class="mx-auto max-w-lg text-center">
     <h1 class="text-2xl font-bold sm:text-3xl">{$_('pages.create_group.Create_Group')}</h1>
   </div>
@@ -54,7 +99,7 @@
     </div>
 
     <div>
-      <label for="descriptoin" class="sr-only">email</label> 
+      <label for="descriptoin" class="sr-only">email</label>
       <div class="relative">
         <input
           bind:value={email}
@@ -65,58 +110,14 @@
         />
       </div>
     </div>
-
-    <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-      {#if status === 'failure'}
-        {#if Object.keys(errors).length > 0}
-          <div role="alert" class="rounded border-l-4 border-red-500 bg-red-50 p-4">
-            <strong class="block font-medium text-red-700"> {$_('pages.create_group.something_went_wrong')} </strong> 
-            <p class="mt-2 text-sm text-red-700">
-              {#each Object.keys(errors) as key}
-              {#if key === 'name'}
-              {$_('pages.create_group.errors.title')}
-                {:else}
-                {key}: {errors[key].join(', ')}  
-              {/if}
-                <br />
-              {/each}
-            </p>
-          </div>
-        {/if}
-      {:else if status === 'success'}
-        <div class="rounded-md bg-green-100 p-4">
-          <div class="flex">
-            <div class="flex-shrink-0">
-              <Icon name="CheckCircle" />
-            </div>
-            <div class="ml-3">
-               <p class="text-sm font-medium text-green-800">{$_('pages.create_group.success_message')}</p>
-            </div>
-            <div class="ml-auto pl-3">
-              <div class="-mx-1.5 -my-1.5">
-                <button
-                  type="button"
-                  class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
-                  on:click|preventDefault={closeAlert}
-                >
-                  <span class="sr-only">Dismiss</span>
-                  <Icon name="XMark" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      {/if}
-
-      <div class="flex items-center justify-between">
-        <button
-          type="submit"
-          on:click={createGroup}
-          class="inline-block rounded-lg bg-rose-500 px-5 py-3 text-sm font-medium text-white"
-        >
+    <div class="flex items-center justify-between">
+      <button
+        type="submit"
+        on:click={createGroup}
+        class="inline-block rounded-lg bg-rose-500 px-5 py-3 text-sm font-medium text-white"
+      >
         {$_('pages.create_group.create_button')}
-        </button>
-      </div>
+      </button>
     </div>
   </form>
 </div>
