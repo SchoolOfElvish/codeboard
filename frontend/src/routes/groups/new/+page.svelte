8e430745e27559faefc892b0646a81df69b21bb4
Pavel <lordevildead@gmail.com>
2023-03-17 16:43:28 +0300

    [COD-38] realise i18n {frontend]

--- frontend/src/routes/groups/new/+page.svelte
+++ frontend/src/routes/groups/new/+page.svelte
@@ -36,7 +36,7 @@
 
 <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
   <div class="mx-auto max-w-lg text-center">
-    <h1 class="text-2xl font-bold sm:text-3xl">Create group:</h1>
+    <h1 class="text-2xl font-bold sm:text-3xl">{$_('pages.create_group.Create_Group')}</h1>
   </div>
 
   <form action="" class="mx-auto mt-8 mb-0 max-w-md space-y-4">
@@ -48,32 +48,36 @@
           minlength="4"
           type="text"
           class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm invalid:border-red-500"
-          placeholder="Enter group title"
+          placeholder={$_('pages.create_group.title')}
         />
       </div>
     </div>
 
     <div>
-      <label for="descriptoin" class="sr-only">email</label>
+      <label for="descriptoin" class="sr-only">email</label> 
       <div class="relative">
         <input
           bind:value={email}
           type="email"
           maxlength="255"
           class="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm invalid:border-red-500"
-          placeholder="Input emails of users you want to invite"
+          placeholder={$_('pages.create_group.email')}
         />
       </div>
     </div>
 
-    <div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0">
+    <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
       {#if status === 'failure'}
         {#if Object.keys(errors).length > 0}
           <div role="alert" class="rounded border-l-4 border-red-500 bg-red-50 p-4">
-            <strong class="block font-medium text-red-700"> Something went wrong </strong>
+            <strong class="block font-medium text-red-700"> {$_('pages.create_group.something_went_wrong')} </strong> 
             <p class="mt-2 text-sm text-red-700">
               {#each Object.keys(errors) as key}
-                {key}: {errors[key].join(', ')}
+              {#if key === 'name'}
+              {$_('pages.create_group.errors.title')}
+                {:else}
+                {key}: {errors[key].join(', ')}  
+              {/if}
                 <br />
               {/each}
             </p>
@@ -86,7 +90,7 @@
               <Icon name="CheckCircle" />
             </div>
             <div class="ml-3">
-              <p class="text-sm font-medium text-green-800">Successfully created</p>
+               <p class="text-sm font-medium text-green-800">{$_('pages.create_group.success_message')}</p>
             </div>
             <div class="ml-auto pl-3">
               <div class="-mx-1.5 -my-1.5">
@@ -110,7 +114,7 @@
           on:click={createGroup}
           class="inline-block rounded-lg bg-rose-500 px-5 py-3 text-sm font-medium text-white"
         >
-          Create Group
+        {$_('pages.create_group.create_button')}
         </button>
       </div>
     </div>
