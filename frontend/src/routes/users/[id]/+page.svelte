f820418361295206e8f0959abcf871dce2583f71
Evgenii <silaewgenii@gmail.com>
2023-03-30 23:12:15 +0300

    [COD-37] BIO output adjustment

--- frontend/src/routes/users/[id]/+page.svelte
+++ frontend/src/routes/users/[id]/+page.svelte
@@ -56,9 +56,9 @@
                 id="applicant-information-title"
                 class="text-lg font-medium leading-6 text-gray-900"
               >
-                {$_(`pages.users.me.about_me`)}
+                Applicant Information
               </h2>
-              <p class="mt-1 max-w-2xl text-sm text-gray-500">{about_info}</p>
+              <p class="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
             </div>
             <div class="border-t border-gray-200 px-4 py-5 sm:px-6">
               <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
@@ -78,15 +78,16 @@
                   <dt class="text-sm font-medium text-gray-500">Phone</dt>
                   <dd class="mt-1 text-sm text-gray-900">+1 555-555-5555</dd>
                 </div>
-                <div class="sm:col-span-2">
-                  <dt class="text-sm font-medium text-gray-500">About</dt>
-                  <dd class="mt-1 text-sm text-gray-900">
-                    Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum
-                    culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla
-                    mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad
-                    adipisicing reprehenderit deserunt qui eu.
-                  </dd>
-                </div>
+                {#if about_info !== null}
+                  <div class="sm:col-span-2">
+                    <dt class="text-sm font-medium text-gray-500">
+                      {$_(`pages.users.me.about_me`)}
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900">
+                      {about_info}
+                    </dd>
+                  </div>
+                {/if}
               </dl>
             </div>
             <div>
