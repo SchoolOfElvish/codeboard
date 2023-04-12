87e36db9afc8b9f08648dc58f286363ada7300fa
Evgenii <silaewgenii@gmail.com>
2023-04-12 20:25:56 +0300

    [COD-47] Changed output style for each notification

--- frontend/src/components/common/Notifications.svelte
+++ frontend/src/components/common/Notifications.svelte
@@ -1,38 +1,7 @@
 <script lang="ts">
   import notifications from '$stores/notifications';
 
-  function addSuccessNotification() {
-    notifications.success({
-      message: 'Success!',
-      description: 'Your action was successful.'
-    });
-  }
-  function addErrorNotification() {
-    notifications.error({
-      message: 'error!',
-      description: 'Your action was error.'
-    });
-  }
-  function addInfoNotification() {
-    notifications.info({
-      message: 'info!',
-      description: 'Your action was info.'
-    });
-  }
-  function addWarningNotification() {
-    notifications.warning({
-      message: 'warning!',
-      description: 'Your action was warning.'
-    });
-  }
 </script>
-
-<div>
-  <button on:click={addSuccessNotification}>success</button>
-  <button on:click={addErrorNotification}>error</button>
-  <button on:click={addInfoNotification}>info</button>
-  <button on:click={addWarningNotification}>warning</button>
-</div>
 
 <div
   class="pointer-events-none fixed inset-0 flex items-end px-4 py-20 sm:items-start bg-opacity-25"
@@ -45,20 +14,67 @@
         <div class="p-4">
           <div class="flex items-start">
             <div class="flex-shrink-0">
-              <svg
-                class="h-6 w-6 text-green-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+              {#if notification.type === 'success'}
+                <svg
+                  class="h-6 w-6 text-green-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              {:else if notification.type === 'error'}
+                <svg
+                  class="h-6 w-6 text-red-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10 14.5V9.5M14 14.5V9.5M3 20.5h18a2 2 0 002-2V6a2 2 0 00-2-2H3a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+              {:else if notification.type === 'warning'}
+                <svg
+                  class="h-6 w-6 text-yellow-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 9v2m0 4h.01m-6.938-4.582a1 1 0 01.184-1.394l5-4a1 1 0 011.272 0l5 4a1 1 0 01.183 1.395l-2.5 2a1 1 0 01-1.237.027L12 11.317l-2.709 2.108a1 1 0 01-1.237-.027l-2.5-2zM6 21h12a3 3 0 003-3V6a3 3 0 00-3-3H6a3 3 0 00-3 3v12a3 3 0 003 3z"
+                  />
+                </svg>
+              {:else}
+                <svg
+                  class="h-6 w-6 text-blue-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 19l-7-7 2.238-2.238L12 14.523l6.762-6.761L19 12z"
+                  />
+                </svg>
+              {/if}
             </div>
             <div class="ml-3 w-0 flex-1 pt-0.5">
               <p class="text-sm font-medium text-gray-900">{notification.message}</p>
