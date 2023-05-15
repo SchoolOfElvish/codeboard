4eed55400dc455ee6d14325b2a1c67707f7feba3
Evgenii <silaewgenii@gmail.com>
2023-05-15 22:35:42 +0300

    [COD-51] Add migrate for confirmable

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_30_160556) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_15_191623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,6 +104,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_30_160556) do
     t.date "birthdate"
     t.datetime "token_issued_at"
     t.text "about_info"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
