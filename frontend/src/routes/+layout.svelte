9ae6605a1ca89651211452c75095349b262602fc
Pavel <lordevildead@gmail.com>
2023-05-31 14:39:27 +0300

    [COD-41] footer ferst verion added [frontend]

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,9 +2,12 @@
   import '../app.css';
   import Header from '$components/common/Header.svelte';
   import Notifications from '$components/common/Notifications.svelte';
+  import Footer from '$components/common/Footer.svelte';
 </script>
 
 <Header />
 
 <Notifications />
 <slot />
+
+<Footer />
