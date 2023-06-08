ff9ecfb3c63a5e02d3bc9391504b3aef41b3da50
Mikhail Zaitsau <mikhail_zaitsau@outlook.com>
2023-06-09 01:20:54 +0300

    [COD-53] Added logic for mapping lessons on a course page(frontend)

--- frontend/src/routes/courses/[id]/+page.svelte
+++ frontend/src/routes/courses/[id]/+page.svelte
@@ -2,25 +2,14 @@
   import { _ } from 'svelte-i18n';
   import type { PageData } from './$types';
   export let data: PageData
-  import { onMount } from 'svelte';
   import { writable } from 'svelte/store';
-  import { get } from '$utils/fetch';
-  import type { Lesson } from '$components/lesson';
-
-  
+    
   let {
     name: name,
     user: user
-  } = data.response;
+  } = data.response_course;
 
-  let lessons = writable<Lesson[]>([]);
-          
-  async function fetchLessons() {
-    const data = await (await get(`v1/lessons`)).json<Lesson[]>();
-    lessons.set(data);
-  }
-
-  onMount(fetchLessons);
+  let lessons = writable(data.response_lessons);
 </script>
 
 <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
@@ -30,26 +19,36 @@
   </div>
 </div>
 
-
-
-<article class="mx-auto max-w-7xl overflow-hidden rounded-lg border border-gray-100 shadow-sm">
-  <div class="p-4 sm:p-6">
-    <a href="/">
-      <h3 class="text-lg font-medium text-gray-900">Lesson 1</h3>
-    </a>
-
-    <p class="mt-2 text-sm leading-relaxed text-gray-500 line-clamp-3">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-      pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem,
-      mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque
-      dignissimos. Molestias explicabo corporis voluptatem?
-    </p>
-
-    <a href="/" class="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
-      Find out more
-
-      <span aria-hidden="true" class="block transition group-hover:translate-x-0.5"> &rarr; </span>
-    </a>
+{#if $lessons.length === 0}
+  <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+    <!-- <p>{$_(`pages.lessons.none_search`)}</p> -->
+    <p>{$_(`No lessons found`)}</p>
   </div>
-</article>
-  
+{:else}
+  {#each $lessons as lesson}
+    <article
+      class="mx-auto max-w-7xl overflow-hidden rounded-lg border border-gray-100 shadow-sm"
+    >
+      <div class="p-4 sm:p-6">
+        <a href="/">
+          <h3 class="text-lg font-medium text-gray-900">{lesson.title}</h3>
+        </a>
+
+        <p class="mt-2 text-sm leading-relaxed text-gray-500 line-clamp-3">
+          {$_(`Description`)}: {lesson.description}
+        </p>
+
+        <a
+          href="/"
+          class="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600"
+        >
+          <!-- {$_(`pages.courses.find`)} -->
+
+          <span aria-hidden="true" class="block transition group-hover:translate-x-0.5">
+            &rarr;
+          </span>
+        </a>
+      </div>
+    </article>
+  {/each}
+{/if}
--- frontend/src/routes/courses/[id]/+page.ts
+++ frontend/src/routes/courses/[id]/+page.ts
@@ -1,11 +1,13 @@
 import { get } from '$utils/fetch';
 import type { Course } from '$components/course';
+import type { Lesson } from '$components/lesson';
 import type { RouteParams } from './$types';
 
 export const ssr = false;
 let errors: string;
 
 export const load = async ({ params }: { params: RouteParams }) => {
-  const response = await (await get(`/v1/courses/${params.id}`)).json<Course>();
-  return { response, errors };
+  const response_course = await (await get(`/v1/courses/${params.id}`)).json<Course>();
+  const response_lessons = await (await get(`/v1/courses/${params.id}/lessons`)).json<Lesson[]>();
+  return { response_course, response_lessons, errors };
 };
